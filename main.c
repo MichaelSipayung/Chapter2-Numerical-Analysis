f7fa99bd7f5330dee39f1058727feb76226d3923
Michael Sipayung <michaelsipayung123@gmail.com>
2021-08-18 16:24:04 +0700

    adding bisection method adding file handling

--- main.c
+++ main.c
@@ -17,12 +17,15 @@ double bisection(double tolerance ,int maxIter,double lower,double upper){
     double fa=f(lower);
     fprintf(bisec,"\t... Bisection Method ... \n");
     while (i<maxIter){
+        if(f(lower)*f(upper) >0){break;}
+        //else
         p=lower+(upper-lower)/2.0;
         fp=f(p);
         fprintf(bisec,"Iteration %d \tf(x) %.7f\n",i, fabs(fp));
         if((fp ==0.0)|| (fabs((upper-lower)/2.0)) < tolerance){
             printf("Total interation \t: %d\n" , i);
-            printf("f(x) value \t: %.6f\n", fabs(fp));
+            printf("f(x) value \t: %.7f\n", fabs(fp));
+            printf("Tolerance \t: %0.7f\n", fabs(upper-lower));
             return p;
             break;
         }
